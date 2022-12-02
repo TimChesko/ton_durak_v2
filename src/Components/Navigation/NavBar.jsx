7f7cdb091b422eebf61b629f2398bd385ea5de69
TimChesko <timofey.lazarev@gmail.com>
2022-12-02 19:50:55 +0300

    Сделал нижнюю навигацию. Переключается между вкладками, меняется цвет иконок и текста.

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import css from "./NavBar.module.css"
 import {NavLink} from "react-router-dom";
+import { ReactComponent as HomeSvg } from './../../Assets/icon_home.svg';
+import { ReactComponent as ProfileSvg } from './../../Assets/icon_profile.svg';
 
 const NavBar = () => {
-    let flag = ({isActive}) => isActive ? css.active : css.nav_menu;
+    let flag = ({isActive}) => isActive ? `${css.nav_menu} ${css.active}` : css.nav_menu;
     return (
         <nav className={css.nav}>
             <div className={css.nav_area}>
                 <div className={css.nav_rectangle}>
                     <div className={css.nav_menu}>
-                        <NavLink to={"/home"}  className={flag}>Главная</NavLink>
-                        <NavLink to={"/profile"} className={flag}>Профиль</NavLink>
+                        <NavLink to={"/home"} className={flag} >
+                            <HomeSvg/>
+                            <a>Главная</a>
+                        </NavLink>
+                        <NavLink to={"/profile"} className={flag}>
+                            <ProfileSvg/>
+                            <a>Профиль</a>
+                        </NavLink>
                     </div>
                 </div>
             </div>
@@ -18,4 +26,4 @@ const NavBar = () => {
     )
 }
 
-export default NavBar
+export default NavBar;
